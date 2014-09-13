9a20795a42ade60413fb4452e929e73a0c04582e
Kai INUI <lied.der.optik@gmail.com>
2014-09-13 23:21:32 +0900

    init

---
+++ KIMultipart/KIViewController.h
@@ -0,0 +1,13 @@
+//
+//  KIViewController.h
+//  KIMultipart
+//
+//  Created by kaiinui on 2014/09/13.
+//  Copyright (c) 2014年 kaiinui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KIViewController : UIViewController
+
+@end
---
+++ KIMultipartTests/KIMultipartTests.m
@@ -0,0 +1,34 @@
+//
+//  KIMultipartTests.m
+//  KIMultipartTests
+//
+//  Created by kaiinui on 2014/09/13.
+//  Copyright (c) 2014年 kaiinui. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KIMultipartTests : XCTestCase
+
+@end
+
+@implementation KIMultipartTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
